bd245c8b47b0831faf2fd8e88ba88d035c2559c6
Brian Pino <brianpino@gmail.com>
2018-02-09 17:53:45 -0800

    Fixed problem with body-parser

---
+++ server/src/routes.js
@@ -0,0 +1,9 @@
+const AuthenticationController = require('./controllers/AuthenticationController');
+
+module.exports = (app) => {
+  app.post('/register', AuthenticationController.register);
+
+  app.get('/status', (req, res) => {
+    res.send({ message: 'Ping' });
+  });
+};
